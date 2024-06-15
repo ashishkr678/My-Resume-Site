b7ccd4f9ceb085383b15134b2909b22fdffa8b50
Ashish Kumar <ashishkumat818@gmail.com>
2024-06-15 16:49:03 +0530

    bbbb

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/kelly-sikkema-CEbyMSUu1mg-unsplash.jpg";
+import image from "../images/programming-computer.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -52,7 +52,7 @@ const projectList = [
     title: "My Resume Site",
     description:
       "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my education, skills, experience and abilities.",
-    url: "https://github.com/ashishkr678/My-Resume",
+    url: "https://github.com/ashishkr678/My-Resume-pdf",
   },
   
 ];
